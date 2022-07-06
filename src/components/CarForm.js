3437eed71c802deb217cf579245b1045cd9c7319
Freextt <olehsam@gmail.com>
2022-07-06 19:56:14 +0300

    Cars_api_practice

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,9 +1,39 @@
-export default function CarForm(){
+import {useForm} from "react-hook-form";
+import {carService} from "../services";
+
+export default function CarForm({addCar}){
+    const {register,handleSubmit,reset,setValue,formState:{errors}} = useForm({mode:'all'});
+    const submit = (newCar) => {
+        const {data} = carService.create(newCar);
+        addCar(data);
+        reset();
+    }
     return(
         <div>
-            <form>
-
+            <form onSubmit={handleSubmit(submit)}>
+                <input type="text" placeholder={'model'} {...register('model', {
+                    required:true,
+                    pattern: new RegExp(/^[a-zA-ZА-яіІїЇ]{1,20}$/)
+                })}/>
+                <input type="number" placeholder={'price'} {...register('price', {
+                    valueAsNumber:true,
+                    required:true,
+                    min: 0,
+                    max: 1000000
+                })}/>
+                <input type="number" placeholder={'year'} {...register('year', {
+                    valueAsNumber:true,
+                    required:true,
+                    min: 1990,
+                    max: new Date().getFullYear()
+                })}/>
+                <button>Save</button>
             </form>
+            <div>
+                {errors.model && <div>Only alphabetic min 1 and max 20</div> }
+                {errors.price && <div>Min 0 and Max 1000000</div>}
+                {errors.year && <div>Min 1990 year Max current year</div>}
+            </div>
         </div>
     )
 }
--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import {carService} from "../services";
 import Car from "./Car";
+import CarForm from "./CarForm";
 
 export default Cars;
 function Cars(){
@@ -8,8 +9,13 @@ function Cars(){
    useEffect(()=> {
        carService.getAll().then(({data}) => setCars(data));
    },[])
+    const addCar = (car) =>{
+       setCars([...cars,car]);
+    }
     return(
         <div>
+            <CarForm addCar={addCar}/>
+            <hr/>
             {cars.map(car => <Car key={car.id} car={car}/>)}
         </div>
     )
