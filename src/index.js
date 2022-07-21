8ac13733f1223616fcaa701e1e2c1044cbdacd8a
Freextt <olehsam@gmail.com>
2022-07-22 00:58:02 +0300

    HW_7_part2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {BrowserRouter} from "react-router-dom";
 
+let store = createStore((state = {users:[],posts:[]},action)=>{
+  switch (action.type){
+      case "Load_Users":
+          return {...state,users: action.payload};//в стейті лежать юзери та пости але в даному випадку тягнуться юзери
+
+      case "Load_Posts":
+          return {...state,posts: action.payload};//тут тягнуться пости
+
+      case "Get_User":
+          return state.users.find(item => item.id == action.payload);
+
+      case "Get_Post":
+          return state.posts.find(item => item.id == action.payload);
+      default:
+          return state;
+  }
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}> {/*сховище глобального стану*/}
+          <BrowserRouter>
+              <App/>
+          </BrowserRouter>
+      </Provider>
   </React.StrictMode>
 );
 
