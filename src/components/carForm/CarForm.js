03f196076cf7b6b83c43c582d2a089962fa0b3c6
Freextt <olehsam@gmail.com>
2022-07-27 09:51:54 +0300

    HW_8

--- src/components/carForm/CarForm.js
+++ src/components/carForm/CarForm.js
@@ -1,19 +1,23 @@
 import {useForm} from "react-hook-form";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
+import {carActions} from "../../redux/slices/car.slice";
 
 
 const CarForm = () => {
       const {reset,register,handleSubmit,setValue} =  useForm();
+      const {createCar} =useSelector(state => state.cars);
       const dispatch = useDispatch();
       useEffect(()=>{
-          if(create){
-
+          if(createCar){
+             setValue('model',createCar.model)
+             setValue("price", createCar.price)
+             setValue('year',createCar.year)
           }
       })
     const submit = async (data) => {
-        if (carForUpdate) {
-            await dispatch(carActions.updateById({id: carForUpdate.id, car: data}))
+        if (createCar) {
+            await dispatch(carActions.updateById({id: createCar.id, car: data}))
         } else {
             await dispatch(carActions.create({car: data}))
         }
@@ -26,6 +30,7 @@ const CarForm = () => {
                 <input type="text" placeholder={'model'} {...register('model')}/>
                 <input type="text" placeholder={'price'} {...register('price')}/>
                 <input type="text" placeholder={'year'} {...register('year')}/>
+                <button>Create</button>
             </form>
         </div>
     )
